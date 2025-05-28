3859c8ea22d7cc66469462de51f39d293f514ad1
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2025-05-29 01:26:34 +0600

    Add routes

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+
+export const PORT = process.env.PORT || 3000;
 
 export const prismaClient = new PrismaClient();
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 import express, { json } from "express";
+import { PORT } from "./config";
+import {
+  handleCreateRoutine,
+  handleDeleteRoutine,
+  handleFindAllRoutines,
+  handleFindRoutine,
+  handleUpdateRoutine,
+} from "./controllers";
 
 const app = express();
 app.use(json());
+app.post("/routines", handleCreateRoutine);
+app.delete("/routines/:id", handleDeleteRoutine);
+app.get("/routines", handleFindAllRoutines);
+app.get("/routines/:id", handleFindRoutine);
+app.put("/routines/:id", handleUpdateRoutine);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
